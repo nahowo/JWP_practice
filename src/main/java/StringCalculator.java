e76de09aa6cef7f5b236f1db1797e5e16476ab92
nahowo <nahowo@naver.com>
2025-03-11 00:37:05 +0900

    3.4장 문자열 계산기 리팩토링

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,42 +1,31 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public String[] checkDelimiter(String targetString) {
-        String delimiter;
-        String[] numbers;
-        if (targetString.matches("^[0-9, ]+$")) {
-            delimiter = ",";
+    public int add(String targetString) {
+        if (targetString.matches("^[ ]+$")) {
+            return 0;
         }
-        else if (targetString.matches("^[0-9:]+$")) {
-            delimiter = ":";
-        }
-        else {
-            int checkStart = targetString.indexOf("//");
-            int checkEnd = targetString.indexOf("\n");
-            if (checkStart != -1 && checkEnd != -1 && checkStart <= checkEnd) {
-                delimiter = targetString.substring(checkStart + 2, checkEnd);
-                if (! targetString.substring(checkEnd + 1).matches("^[0-9" + Pattern.quote(delimiter) + "]+$")) {
-                    throw new RuntimeException();
-                }
-                targetString = targetString.substring(checkEnd + 1);
-            }
-            else {
-                throw new RuntimeException();
-            }
-        }
-        numbers = targetString.split(delimiter);
-        return numbers;
+        return sum(splitString(targetString));
     }
 
-    public int add(String[] numbers) {
+    private String[] splitString(String targetString) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(targetString);
+        if (matcher.find()) {
+            return matcher.group(2).split(matcher.group(1));
+        }
+        return targetString.split(",|:");
+    }
+
+    private int sum(String[] numbers) {
         int sum = 0;
+        int tmp;
         for (String i : numbers) {
-            try {
-                sum += Integer.valueOf(i);
+            tmp = Integer.valueOf(i);
+            if (tmp < 0) {
+                throw new RuntimeException();
             }
-            catch (NumberFormatException e) {
-                continue;
-            }
+            sum += tmp;
         }
         return sum;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -10,17 +10,27 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void checkAddTest() {
-        String[] ret1 = calculator.checkDelimiter("//!\n2!28!3");
-        Assertions.assertEquals(33, calculator.add(ret1));
+    public void add_null() {
+        Assertions.assertEquals(0, calculator.add(" "));
+    }
 
-        String[] ret2 = calculator.checkDelimiter("3,4,5");
-        Assertions.assertEquals(12, calculator.add(ret2));
+    @Test
+    public void add_single_number() {
+        Assertions.assertEquals(1, calculator.add("1"));
+    }
 
-        String[] ret3 = calculator.checkDelimiter(" ");
-        Assertions.assertEquals(0, calculator.add(ret3));
+    @Test
+    public void add_number_with_default_delimiter() {
+        Assertions.assertEquals(6, calculator.add("1,2,3"));
+    }
 
-        Assertions.assertThrows(RuntimeException.class, () -> calculator.checkDelimiter("3-2:1"));
-        Assertions.assertThrows(RuntimeException.class, () -> calculator.checkDelimiter("//!\n2-3!23"));
+    @Test
+    public void add_number_with_custom_delimiter() {
+        Assertions.assertEquals(6, calculator.add("//!\n1!2!3"));
+    }
+
+    @Test
+    public void add_negative_number() {
+        Assertions.assertThrows(RuntimeException.class, () -> calculator.add("3,-2,4"));
     }
 }
