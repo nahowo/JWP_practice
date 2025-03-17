151f185f4c1db3908034247fb794ad1561e8ae3b
nahowo <nahowo@naver.com>
2025-03-17 20:38:52 +0900

    GET 방식으로 회원가입하기

--- src/main/java/web_application_server/webserver/RequestHandler.java
+++ src/main/java/web_application_server/webserver/RequestHandler.java
@@ -2,14 +2,18 @@ package web_application_server.webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web_application_server.model.User;
+import web_application_server.util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
+    private static HttpRequestUtils httpRequestUtils;
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
@@ -37,7 +41,14 @@ public class RequestHandler extends Thread {
         }
     }
 
-
+    public User signUp(String line) {
+        int index = line.indexOf("?");
+        String requestUrl = line.substring(0, index);
+        String param = line.substring(index + 1);
+        Map<String, String> data = httpRequestUtils.parseQueryString(param);
+        User user = new User(data.get("userId"), data.get("password"), data.get("name"), data.get("email"));
+        return user;
+    }
 
     public String splitUrl(String line) {
         String[] tokens = line.split(" ");
--- src/test/java/RequestHandlerTest.java
+++ src/test/java/RequestHandlerTest.java
@@ -1,14 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import web_application_server.util.HttpRequestUtils;
 import web_application_server.webserver.RequestHandler;
 import web_application_server.model.User;
 
 import java.net.Socket;
+import java.util.Map;
 
 public class RequestHandlerTest {
     private RequestHandler requestHandler;
-    private User user;
+    private static HttpRequestUtils httpRequestUtils;
     @BeforeEach
     public void setUp() {
         requestHandler = new RequestHandler(new Socket());
@@ -16,6 +18,16 @@ public class RequestHandlerTest {
     @Test
     public void getUrlTest() {
         Assertions.assertEquals("index.html", requestHandler.splitUrl("GET /index.html HTTP/1.1"));
+    }
+
+    @Test
+    public void parseQueryStringTest() {
+        String line = "user/create?userId=nahowo&password=1234&name=nahyun+park&email=nahowo%40naver.com";
+        User user = requestHandler.signUp(line);
+        Assertions.assertEquals("nahowo", user.getUserId());
+        Assertions.assertEquals("1234", user.getPassword());
+        Assertions.assertEquals("nahyun+park", user.getName());
+        Assertions.assertEquals("nahowo%40naver.com", user.getEmail());
     }
 
     @Test
