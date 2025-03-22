0296dd61ad664fe8084d2f48e5ffd600579e445b
nahowo <nahowo@naver.com>
2025-03-22 23:43:20 +0900

    gitignore 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS USERS;
+
+CREATE TABLE USERS
+(
+    userId   varchar(12) NOT NULL,
+    password varchar(12) NOT NULL,
+    name     varchar(20) NOT NULL,
+    email    varchar(50) NOT NULL,
+
+    PRIMARY KEY (userId)
+);
+
+INSERT INTO USERS
+VALUES ('admin', 'password', 'admin', 'admin@naver.com');
